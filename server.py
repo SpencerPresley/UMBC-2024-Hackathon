e19d814febc5fa1c67b26cf16b4847b6f1df9bd6
idugan100 <isaacd4444@gmail.com>
2024-09-28 12:29:29 -0400

    basic form

--- server.py
+++ server.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/")
 async def read_index():
     return FileResponse('form.html')
+
+
+
+
+@app.get("/final")
+async def final():
+    # get have form values
+    # mess with files
+    # llm stuff
+    # finshed test/answer key
+    
+    return FileResponse('finshed-test.html')
