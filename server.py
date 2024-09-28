3811ef6f19f4d7ae575b84838abaa7bc8f9a9547
idugan100 <isaacd4444@gmail.com>
2024-09-28 14:26:12 -0400

    getting form to work with backend

--- server.py
+++ server.py
@@ -1,35 +1,9 @@
 from typing import Annotated
+from typing import List
+
 from starlette.responses import FileResponse
-from fastapi import FastAPI, UploadFile, Form
-from pydantic import BaseModel
-from enum import Enum
+from fastapi import FastAPI, UploadFile, Form, File
 
-class School_Type(Enum):
-    Primary = 1
-    Elementary = 2
-    Middle = 3
-    High = 4
-    Undergraduate = 5
-    Graduate = 6
-
-class Difficulty(Enum):
-    Easy = 1
-    Medium = 2
-    Hard = 3
-    VeryHard = 4
-
-
-class FormSettings(BaseModel):
-    title: str
-    course: str
-    professor: str
-    number_of_mcq_questions: int
-    number_of_TF_questions: int
-    number_of_written_questions: int
-    school_type: School_Type
-    difficulty: Difficulty
-    testing_philosophy: str
-    subject_material: list[UploadFile]
 
 app = FastAPI()
 
@@ -37,14 +11,24 @@ app = FastAPI()
 def read_index():
     return FileResponse('form.html')
 
-@app.get("/generate")
+@app.post("/generate")
 def final(
-    data: FormSettings = Form(...),
+    title: str = Form(...),
+    course: str = Form(...),
+    professor: str = Form(...),
+    number_of_mcq_questions: int = Form(...),
+    number_of_written_questions: int = Form(...),
+    number_of_TF_questions: int = Form(...),
+    school_type: str = Form(...),
+    testing_philosophy: str = Form(...),
+    difficulty: str = Form(...),
+    subject_material: list[UploadFile] = File(...)
     
 ):
+    print("hi")
     # get have form values
     # mess with files
     # llm stuff
     # finshed test/answer key
 
-    return FileResponse('finshed-test.html')
+    return FileResponse('finshed_test.html')
