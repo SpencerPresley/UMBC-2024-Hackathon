9c11288c5cb8f5f9fd0119959b30051cf4d540de
Spencer Presley <153392395+SpencerPresley@users.noreply.github.com>
2024-09-28 15:38:26 -0400

    chain to clean up and reformat docs prior to going to test maker

---
+++ pythonBackend/empty
@@ -0,0 +1,1 @@
+from langchain_openai import Chat
--- pythonBackend/main_ai.py
+++ pythonBackend/main_ai.py
@@ -3,31 +3,38 @@ import os
 import json
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-pdf_file_path = os.path.join(current_directory, 'files', 'Example2.pdf')
+pdf_file_path = os.path.join(current_directory, "files", "ExampleFileForDev.pdf")
 
-loader = PyPDFLoader (
-    file_path = pdf_file_path,
-    extract_images=True
+loader = PyPDFLoader(
+    file_path=pdf_file_path,
+    extract_images=True,
 )
 
 docs = []
-docs_lazy = loader.lazy_load()
+docs_lazy = loader.load_and_split()
+# for doc in docs_lazy:
+#     print(doc)
+#     input("Press Enter to continue...")
+#     # docs.append(doc.page_content)
+
+
+dict_data = {}
+doc_str = "".join([doc.page_content for doc in docs_lazy])
+dict_data["data"] = doc_str
+with open("data.json", "w") as f:
+    json.dump(dict_data, f, indent=4)
+
+pages = []
+metadata = []
+
 for doc in docs_lazy:
-    docs.append(doc)
+    pages.append(doc.page_content)
+    metadata.append(doc.metadata)
 
-print(docs)
-    
-content_string = ''
+# print(pages)
 
-for doc in docs:
-    content_string += doc.page_content
-    print(doc.page_content)
-    
-print(content_string)  
-    
 content_dict = {}
-content_dict['content'] = content_string
-with open('content.json', 'w') as f:
-    json.dump(content_string, f, indent=4)
-
-    
+content_dict["content"] = pages
+content_dict["metadata"] = metadata
+with open("content.json", "w") as f:
+    json.dump(content_dict, f, indent=4)
